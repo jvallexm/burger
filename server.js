df047eb44c47556b65285aaae2bdf35e3c0bd9d9
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-03-13 20:03:44 -0400

    Now serves files

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 const express    = require("express");
-const bodyParser = require("body-parser");
-const path       = require("path");
-const orm        = require(`./config/orm.js`);
-
+const burger     = require("./models/burger.js");
 const port       = process.env.PORT || 8080; // Initialize the port
 const app        = express();                // Initializes express
+const bodyParser = require("body-parser");
+const path       = require("path");
+const exphbs     = require("express-handlebars");
 
 app.listen(port, ()=> console.log(`listening on port ${port}`)); // I hear you, dog
 
 app.use( bodyParser.urlencoded({ extended: false }) ); 
 app.use( bodyParser.json()                          );
-app.use( "/", express.static('app/public')          ); // Expresses the public file as static
+app.use(express.static(path.join(__dirname, 'public')));
 
-orm.updateOne("burgers",{id: 1},{devoured: 1},(r)=>{
-    console.log("ding");
-    orm.selectAll("burgers",(r)=>console.log(r));
-})
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.get("/",(req,res)=>{
+    res.render("index",{hot: "poppers"});
+});
+
+app.get('/test',(req,res)=>{
+    res.sendFile( path.join(__dirname + `/public/test.html`));
+});
